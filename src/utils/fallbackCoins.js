4df4ff2b4c18fa781abe9be3237ccb203de9d0d9
hiro <zhang0824.dev@gmail.com>
2025-03-19 03:01:36 -0700

    fix: Refactore the Code

---
+++ src/utils/fallbackCoins.js
@@ -0,0 +1,54 @@
+const fallbackCoins = {
+    "BTC": { 
+        "CoinName": "Bitcoin", 
+        "Symbol": "BTC",
+        "ImageUrl": "/media/37746251/btc.png" 
+    },
+    "ETH": { 
+        "CoinName": "Ethereum", 
+        "Symbol": "ETH",
+        "ImageUrl": "/media/37746238/eth.png" 
+    },
+    "SOL": { 
+        "CoinName": "Solana", 
+        "Symbol": "SOL",
+        "ImageUrl": "/media/20560/sol.png" 
+    },
+    "DOGE": { 
+        "CoinName": "Dogecoin", 
+        "Symbol": "DOGE",
+        "ImageUrl": "/media/37746339/doge.png" 
+    },
+    "XRP": { 
+        "CoinName": "Ripple", 
+        "Symbol": "XRP",
+        "ImageUrl": "/media/38553096/xrp.png" 
+    },
+    "ADA": { 
+        "CoinName": "Cardano", 
+        "Symbol": "ADA",
+        "ImageUrl": "/media/37746235/ada.png" 
+    },
+    "AVAX": { 
+        "CoinName": "Avalanche", 
+        "Symbol": "AVAX",
+        "ImageUrl": "/media/39383076/avax.png" 
+    },
+    "DOT": { 
+        "CoinName": "Polkadot", 
+        "Symbol": "DOT",
+        "ImageUrl": "/media/39334571/dot.png" 
+    },
+    "LINK": { 
+        "CoinName": "Chainlink", 
+        "Symbol": "LINK",
+        "ImageUrl": "/media/37746242/link.png" 
+    },
+    "LTC": { 
+        "CoinName": "Litecoin", 
+        "Symbol": "LTC",
+        "ImageUrl": "/media/37746243/ltc.png" 
+    }
+};
+
+module.exports = fallbackCoins;
